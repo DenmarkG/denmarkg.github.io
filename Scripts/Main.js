9f57d7bf330e647e6cf9cd0d96480c1283a2051c
Denmark <denmark.s.gibbs@gmail.com>
2018-11-21 12:10:32 -0800

    updated lfs to not include javascript files

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:17dd45c82c7ad8ffb036eb9d289558323167c6f797b641a75a0f9b0f9f72e09b
-size 30
+console.log("Main.js loaded");
+console.log("Main.js test");
+
