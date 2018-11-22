81c98833ba131a2995face5be81b0030e46de78d
Superdenny <denmark.s.gibbs@gmail.com>
2018-11-22 08:20:20 -0800

    Updated javsscript files to stop being tracked by LFS

--- Pages/VectorIntercept/TemplateData/UnityProgress.js
+++ Pages/VectorIntercept/TemplateData/UnityProgress.js
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d0f54aef33f34fd123d1ebc048cf152e01294cb71809ce276e3e1304ba4e2830
-size 1186
+function UnityProgress(gameInstance, progress) {
+  if (!gameInstance.Module)
+    return;
+  if (!gameInstance.logo) {
+    gameInstance.logo = document.createElement("div");
+    gameInstance.logo.className = "logo " + gameInstance.Module.splashScreenStyle;
+    gameInstance.container.appendChild(gameInstance.logo);
+  }
+  if (!gameInstance.progress) {    
+    gameInstance.progress = document.createElement("div");
+    gameInstance.progress.className = "progress " + gameInstance.Module.splashScreenStyle;
+    gameInstance.progress.empty = document.createElement("div");
+    gameInstance.progress.empty.className = "empty";
+    gameInstance.progress.appendChild(gameInstance.progress.empty);
+    gameInstance.progress.full = document.createElement("div");
+    gameInstance.progress.full.className = "full";
+    gameInstance.progress.appendChild(gameInstance.progress.full);
+    gameInstance.container.appendChild(gameInstance.progress);
+  }
+  gameInstance.progress.full.style.width = (100 * progress) + "%";
+  gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+  if (progress == 1)
+    gameInstance.logo.style.display = gameInstance.progress.style.display = "none";
+}
