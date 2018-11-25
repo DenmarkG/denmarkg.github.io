a98ea0181a883ca134763d43e74ec67e3452afc6
Superdenny <denmark.s.gibbs@gmail.com>
2018-11-24 19:44:31 -0800

    UPdated header file

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,20 +1,20 @@
 console.log("Main.js loaded");
 
-function loadHeader()
-{
-    let headerParent = document.getElementById("header");
+// function loadHeader()
+// {
+//     let headerParent = document.getElementById("header");
 
-    let header = document.createElement('object');
-    header.innerHTML = '<object type="text/html" data="HTML/Header.html" ></object>';
+//     let header = document.createElement('object');
+//     header.innerHTML = '<object type="text/html" data="HTML/Header.html" ></object>';
     
-    headerParent.appendChild(header);
-    console.log('header loaded' + header);
-}
+//     headerParent.appendChild(header);
+//     console.log('header loaded' + header);
+// }
 
 $(document).ready(function()
 {
     let header = document.getElementById('header');
     
-    $('#header').load('https://superdenny.github.io/HTML/Header.html');
+    $('#header').load('/HTML/Header.html');
     console.log('header loaded');
 });
