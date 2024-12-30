be6578d9a83c1e3ada36a25b17a53c26ac26db43
DenmarkG <denmark.s.gibbs@gmail.com>
2024-12-29 19:56:10 -0800

    [WIP] Add info modals to shipped titles

--- Scripts/index.js
+++ Scripts/index.js
@@ -24,15 +24,76 @@ function navSlide()
             }
         });
 
-
         // Burger Animation
         burger.classList.toggle('burger-toggle');
     });
 }
 
-function init()
+let modalText = "";
+let shippedTitles = [];
+
+function showModal(modal)
+{
+    // modal.classList.toggle('modal-visible');
+    modal.style.display = "flex";
+}
+
+function hideModal(modal)
+{
+    modal.style.display = "none";
+}
+
+function initModal()
+{
+    const modal = document.querySelector('.modal');
+    const cards = document.querySelectorAll('.card');
+
+    modal.onclick = function()
+    {
+        hideModal(modal);
+    };
+
+    cards.forEach((card, index) => 
+    {
+        card.onclick = function()
+        {
+            let modalText = modal.querySelector('#modalText');
+            modalText.innerHTML = shippedTitles[index].Title;
+            showModal(modal);
+        };
+    });
+}
+
+async function loadData()
+{
+    const url = './Data/Shipped.json';
+    try 
+    {
+        const response = await fetch(url);
+        if (!response.ok) 
+        {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+
+        for (let i in json.titles) 
+        {
+            shippedTitles.push(json.titles[i]);
+        } 
+    } 
+    catch (error) 
+    {
+        console.error(error.message);
+    }
+}
+
+async function init()
 {
     navSlide();
+    await loadData();
+    initModal();
 }
 
 init();
