4447661990a6922b4856bd355fb28c599c3854f3
Superdenny <denmark.s.gibbs@gmail.com>
2025-02-13 21:28:16 -0800

    [UPDATE] First pass adding resources page

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,38 +1,6 @@
 const addEmbedMedia = true;
 const kDataFolder = './Data/'
 
-
-function navSlide()
-{
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    // Toggle Nav
-    burger.addEventListener('click', () => 
-    {
-        // Lock scroll
-        document.body.classList.toggle('lock');
-        nav.classList.toggle('nav-active');
-
-        // Animate Links
-        navLinks.forEach((link, index) => 
-        {
-            if (link.style.animation) 
-            {
-                link.style.animation = '';
-            }
-            else 
-            {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`
-            }
-        });
-
-        // Burger Animation
-        burger.classList.toggle('burger-toggle');
-    });
-}
-
 const modal = document.querySelector('.modal');
 let modalText = "";
 let shippedTitles = [];
@@ -67,7 +35,6 @@ async function loadJsonFile(url)
         }
 
         const json = await response.json();
-        console.log(json);
 
         return json
     }
@@ -77,27 +44,25 @@ async function loadJsonFile(url)
     }
 }
 
-async function loadHtmlFile(url) 
-{
-    console.log('Loading file ' + url);
-    try 
-    {
-        const response = await fetch(url);
-        if (!response.ok) 
-        {
-            throw new Error(`Response status: ${response.status}`);
-        }
+// async function loadHtmlFile(url) 
+// {
+//     try 
+//     {
+//         const response = await fetch(url);
+//         if (!response.ok) 
+//         {
+//             throw new Error(`Response status: ${response.status}`);
+//         }
 
-        const txt = await response.text();
-        console.log(txt);
+//         const txt = await response.text();
 
-        return txt;
-    }
-    catch (error) 
-    {
-        console.error(error.message);
-    }
-}
+//         return txt;
+//     }
+//     catch (error) 
+//     {
+//         console.error(error.message);
+//     }
+// }
 
 async function setupCardHtml(txt, cardData)
 {
@@ -177,7 +142,6 @@ async function loadData()
 
 async function init()
 {
-    navSlide();
     await loadData();
     initModal();
 }
